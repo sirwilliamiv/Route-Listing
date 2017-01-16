9e6ebb1551d9148a802c379d5489d66221f85280
sirwilliamiv <wjconnolly2@gmail.com>
2017-01-16 14:37:28 -0600

    added list page of links for homepage/ set otherwise to return 404 error

--- app.js
+++ app.js
@@ -15,16 +15,33 @@ app.config( function($routeProvider) {
     controller: 'SecondCtrl',
     templateUrl: 'route2.html'
   })
+  .when('/', {
+    controller: 'ListingCtrl',
+    templateUrl: 'listing.html'
+  })
+  .when('/404', {
+
+    templateUrl: '404.html'
+  })
+  .otherwise({
+    redirectTo: '/404'
+  })
+
 })
 
 
 //Controllers
 app.controller('FirstCtrl', function($scope) {
   $scope.highwayName = "Blue Ridge Parkway"
-$scope.highwayDescription = "The Blue Ridge Parkway is a National Parkway and All-American Road in the United States, noted for its scenic beauty."
+  $scope.highwayDescription = "The Blue Ridge Parkway is a National Parkway and All-American Road in the United States, noted for its scenic beauty."
 })
 
 app.controller('SecondCtrl', function($scope) {
   $scope.highwayName = "Route 66"
   $scope.highwayDescription = "U.S. Route 66, also known as the Will Rogers Highway, the Main Street of America or the Mother Road, was one of the original highways within the U.S. Highway System."
+})
+
+app.controller('ListingCtrl', function($scope) {
+  // $scope.blue = Blue Ridge Parkway
+  // $scope.route = Route 66
 })
