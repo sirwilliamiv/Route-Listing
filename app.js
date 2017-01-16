8584fae5273a69589e51b4d8a2d48d8663ff3e0a
sirwilliamiv <wjconnolly2@gmail.com>
2017-01-16 14:11:00 -0600

    working exercise

---
+++ app.js
@@ -0,0 +1,30 @@
+// console.log("hey"
+//
+
+var app = angular.module('bestHighways', ['ngRoute'])
+
+
+//Routes
+app.config( function($routeProvider) {
+  $routeProvider
+  .when('/blueridgeparkway', {
+    controller:'FirstCtrl',
+    templateUrl:'route1.html'
+  })
+  .when('/route66', {
+    controller: 'SecondCtrl',
+    templateUrl: 'route2.html'
+  })
+})
+
+
+//Controllers
+app.controller('FirstCtrl', function($scope) {
+  $scope.highwayName = "Blue Ridge Parkway"
+$scope.highwayDescription = "The Blue Ridge Parkway is a National Parkway and All-American Road in the United States, noted for its scenic beauty."
+})
+
+app.controller('SecondCtrl', function($scope) {
+  $scope.highwayName = "Route 66"
+  $scope.highwayDescription = "U.S. Route 66, also known as the Will Rogers Highway, the Main Street of America or the Mother Road, was one of the original highways within the U.S. Highway System."
+})
